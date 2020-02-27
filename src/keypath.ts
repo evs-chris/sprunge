afa1370ef7df8eec46dddd17edc19a746c51c7cb
Chris Reeves <evschris@gmail.com>
2020-02-27 14:39:26 -0500

    add a keypath to key array grammar

---
+++ src/keypath.ts
@@ -0,0 +1,8 @@
+import { parser, map, rep1, rep, seq, str, alt, bracket } from './index';
+import { concat, JNum, JString, JIdentifier, JStringEscape } from './json';
+
+export const Identifier = map(rep1(alt(JIdentifier, JStringEscape)), r => concat(r));
+export const DotPath = map(seq(str('.'), Identifier), r => r[1]);
+export const BracketPath = bracket(str('['), alt<number|string>(JNum, JString), str(']'))
+export const Keypath = map(seq(JIdentifier, rep(alt(DotPath, BracketPath))), r => { r[1].unshift(r[0]); return r[1] });
+export const parse = parser(Keypath, { consumeAll: true });

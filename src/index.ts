3dcbc6c48d3f5c982f016f9e24fe55e580b37dd0
Chris Reeves <evschris@gmail.com>
2020-07-30 13:32:34 -0400

    avoid empty allocation in rep and repsep

--- src/index.ts
+++ src/index.ts
@@ -702,11 +702,24 @@ export function getLatestCause(causes: Cause[], outer: Cause): Cause {
  */
 export function rep<T>(parser: Parser<T>): IParser<T[]> {
   let ps: IParser<T>;
+  const empty: T[] = [];
   function parse(s: string, p: number, resin?: Success<T[]>): Result<T[]> {
     resin = resin || [null, 0];
-    let seq: T[] = [];
+    let seq: T[];
     let c = p;
     let res: Result<T>;
+
+    // unroll the first pass to avoid weird allocation
+    res = ps.parse(s, c, resin as any);
+    if (!res.length) {
+      resin[0] = empty;
+      resin[1] = c;
+      return resin;
+    } else {
+      c = res[1];
+      seq = [res[0]];
+    }
+
     while (1) {
       res = ps.parse(s, c, resin as any);
       if (res.length) {
@@ -800,6 +813,17 @@ export function str(...strings: string[]): IParser<string> {
           } else return fail(p, detailedFail && `expected '${str}'`);
         }
       }
+    } else if (len === 2) {
+      return {
+        parse(s: string, p: number, res?: Success<string>) {
+          if (s[p] === str[0] && s[p + 1] === str[1]) {
+            res = res || ['', 0];
+            res[0] = str;
+            res[1] = p + 2;
+            return res;
+          } else return fail(p, detailedFail && `expected '${str}'`);
+        }
+      }
     } else {
       let i: number, j: number;
       return {
@@ -879,19 +903,46 @@ export function opt<T>(parser: Parser<T>): IParser<null|T> {
 export function repsep<T>(parser: Parser<T>, sep: Parser<any>, trail: 'allow'|'disallow'|'require' = 'disallow'): IParser<T[]> {
   let ps1: IParser<T>;
   let ps2: IParser<any>;
+  const empty: T[] = [];
   function parse(s: string, p: number, resin?: Success<T[]>): Result<T[]> {
     resin = resin || [null, 0];
-    let seq: T[] = [];
+    let seq: T[];
     let c = p;
     let m = p;
     let rr: T;
+    let res: Result<T>;
+    let r: Result<any>;
+
+    // unroll first iteration to avoid weird allocation
+    res = ps1.parse(s, c, resin as any);
+    if (res.length) {
+      rr = res[0];
+      m = c;
+      c = res[1];
+      r = ps2.parse(s, c, resin as any);
+      if (!r.length) {
+        if (trail === 'require') return fail(m, detailedFail && `expected separator`);
+        resin[0] = [rr];
+        resin[1] = c;
+        return resin;
+      } else {
+        c = r[1];
+      }
+
+      seq = [rr];
+    } else {
+      resin[0] = empty;
+      resin[1] = p;
+      return resin;
+    }
+
     while (1) {
-      const res = ps1.parse(s, c, resin as any);
+      res = ps1.parse(s, c, resin as any);
       if (res.length) {
         m = c;
         c = res[1];
         rr = res[0];
-        const r = ps2.parse(s, c, resin as any);
+        r = ps2.parse(s, c, resin as any);
         if (!r.length) {
           if (trail === 'require') return fail(m, detailedFail && `expected separator`);
           seq.push(rr);

7d20ab174dc4182255056e839f4182f8f53e04ac
Chris Reeves <evschris@gmail.com>
2020-11-28 18:04:53 -0500

    add istr parser for case-insensitive str matching

--- src/parsers/str.ts
+++ src/parsers/str.ts
@@ -1,4 +1,5 @@
 import { IParser, Success, fail, detailedFail } from '../base';
+import { read1 } from './char';
 
 /**
  * Creates a parser that reads one of the given strings.
@@ -69,4 +70,27 @@ export function str(...strings: string[]): IParser<string> {
       }
     };
   }
+}
+
+/**
+ * Creates a parser that reads one of the given strings in any case and returns it in the given case.
+ * This is more expensive than str, as it involves matching any chars in the input strings in either
+ * case and then verifying the matched content against the list using indexOf.
+ *
+ * @param strings - the list of strings to match
+ */
+export function istr(...strings: string[]): IParser<string> {
+  const copy = strings.slice();
+  const chars = read1(copy.map(s => s.toLowerCase() + s.toUpperCase()).join(''));
+  const idx = copy.map(s => s.toLowerCase());
+  return {
+    parse(s: string, p: number, res?: Success<string>) {
+      const r = chars.parse(s, p, res || ['', 0]);
+      if (!r.length) return r;
+      const i = idx.indexOf(r[0].toLowerCase());
+      if (!~i) return fail(p, detailedFail && `expected ${copy.length > 1 ? 'one of ' : ''}${copy.map(s => `'${s}'`).join(', ')}`);
+      r[0] = copy[i];
+      return r;
+    }
+  };
 }
